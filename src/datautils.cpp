451c554dacd4c191051f9f47b2fb77094ca0a95c
Flavio <flavio@odisseo.local>
2013-09-21 13:47:04 +0200

    Don't sort "hashes"

--- src/datautils.cpp
+++ src/datautils.cpp
@@ -2,51 +2,26 @@
 
 DataUtils::DataUtils() { }
 
-static const QRegExp nonWordExpression("\\W+");
-// static const QRegExp nonWordExpression("[^\\p{L}]+");
-
-/**
-  * Clean a string removing all non-word chars
-  * and fixing all uppercase chars
-  * This function is intended to increase legibility
-  */
 QString DataUtils::cleanTag(QString s) {
-
     s.replace("_", " ");
-    s = s.simplified();
-
-    return s;
+    return s.simplified();
 }
 
-/**
-  * Build a unique identifier for a string
-  */
 QString DataUtils::normalizeTag(QString s) {
+    static const QRegExp nonWordExpression("\\W+");
+    // static const QRegExp nonWordExpression("[^\\p{L}]+");
+
     s = s.simplified().toLower();
 
     // The Beatles => Beatles
     s.replace(QRegExp("^the "), "");
 
-    // sort words
-    s.replace("_", "");
-    // s.replace("&", "and");
-    QStringList words = s.split(nonWordExpression, QString::SkipEmptyParts);
-    words.sort();
-    s = words.join("");
-
     // remove anything that is not a "letter"
-    // s = s.replace(nonWordExpression, "");
-
-    // s = s.simplified().toLower();
-
-    // TODO simplify accented chars èé=>e etc
+    s.remove(nonWordExpression);
 
     return s;
 }
 
-/**
-  * Md5 of a string
-  */
 QString DataUtils::md5(const QString name) {
     return QString::fromAscii(QCryptographicHash::hash(name.toUtf8(), QCryptographicHash::Md5).toHex());
 }
