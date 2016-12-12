d3eb32a8d5bb7c31149864ae703e99990efef083
Flavio Tordini <flavio.tordini@gmail.com>
2016-12-12 22:24:11 +0100

    Fix crash

--- src/appwidget.cpp
+++ src/appwidget.cpp
@@ -39,7 +39,7 @@ void AppsWidget::paintEvent(QPaintEvent *e) {
     style()->drawPrimitive(QStyle::PE_Widget, &o, &p, this);
 }
 
-AppWidget::AppWidget(const QString &name, const QString &code, QWidget *parent) : QWidget(parent), icon(0), name(name) {
+AppWidget::AppWidget(const QString &name, const QString &code, QWidget *parent) : QWidget(parent), icon(0), name(name), downloadButton(0) {
     const QString unixName = code.left(code.lastIndexOf('.'));
     const QString baseUrl = QLatin1String("http://") + Constants::ORG_DOMAIN;
     const QString filesUrl = baseUrl + QLatin1String("/files/");
@@ -81,16 +81,12 @@ AppWidget::AppWidget(const QString &name, const QString &code, QWidget *parent) 
 
 void AppWidget::enterEvent(QEvent *e) {
     Q_UNUSED(e);
-#if !defined(APP_UBUNTU) && !defined(APP_MAC_STORE)
-    downloadButton->show();
-#endif
+    if (downloadButton) downloadButton->show();
 }
 
 void AppWidget::leaveEvent(QEvent *e) {
     Q_UNUSED(e);
-#if !defined(APP_UBUNTU) && !defined(APP_MAC_STORE)
-    downloadButton->hide();
-#endif
+    if (downloadButton) downloadButton->hide();
 }
 
 void AppWidget::mouseReleaseEvent(QMouseEvent *e) {
