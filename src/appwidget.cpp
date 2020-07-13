63832b6f48b24e92374ec808acb92c7c92068579
Flavio Tordini <flavio.tordini@gmail.com>
2020-07-13 10:58:38 +0200

    useless initializer

--- src/appwidget.cpp
+++ src/appwidget.cpp
@@ -43,7 +43,7 @@ void AppsWidget::paintEvent(QPaintEvent *e) {
 }
 
 AppWidget::AppWidget(const QString &name, const QString &code, QWidget *parent)
-    : QWidget(parent), icon(nullptr), name(name), downloadButton(nullptr) {
+    : QWidget(parent), name(name), downloadButton(nullptr) {
     const QString unixName = code.left(code.lastIndexOf('.'));
     const QString baseUrl = QLatin1String("https://") + Constants::ORG_DOMAIN;
     const QString filesUrl = baseUrl + QLatin1String("/files/");
